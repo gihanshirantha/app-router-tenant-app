f8eba63e0a4cc9fdb03a42320d4b63dbcf7fe1d1
Gihan Shirantha <shirantha1999@gmail.com>
2023-12-07 16:03:24 +0530

    patch: Implemet the metadata

--- src/app/(general)/(home)/page.tsx
+++ src/app/(general)/(home)/page.tsx
@@ -1,6 +1,12 @@
 import { getAllProducts } from "@/Services/Product/product.service";
 import { HomeTemplate } from "@/ui-core";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Modern-walk - Trendy Clothes for Every Style",
+  description: "Discover the latest trends in fashion at Modern-walk",
+};
 
 async function Home() {
   const data = await getAllProducts();
--- src/app/(general)/mensFashion/page.tsx
+++ src/app/(general)/mensFashion/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 import { getProductsInCategory } from "@/Services/Product/product.service";
 import { Categorytemplate } from "@/ui-core";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Men's Clothes",
+  description:
+    "Explore the latest trends in men's fashion at Modern-walk. Find a wide range of stylish men's clothes and accessories",
+};
 
 async function MensFashion() {
   const data = await getProductsInCategory("men's clothing");
