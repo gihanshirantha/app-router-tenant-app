3621f223f9b3bc5a47f8dea1eb7de45e9fe2913e
Gihan Shirantha <shirantha1999@gmail.com>
2023-12-12 16:20:47 +0530

    patch: fix bugs

--- src/app/(general)/(home)/page.tsx
+++ src/app/(general)/(home)/page.tsx
@@ -3,11 +3,6 @@ import { HomeTemplate } from "@/ui-core";
 import { Metadata } from "next";
 import React from "react";
 
-export const metadata: Metadata = {
-  title: "Modern-walk - Trendy Clothes for Every Style",
-  description: "Discover the latest trends in fashion at Modern-walk",
-};
-
 async function Home() {
   const data = await getAllProducts();
   return (
--- src/app/(general)/mensFashion/page.tsx
+++ src/app/(general)/mensFashion/page.tsx
@@ -1,20 +1,18 @@
-"use client";
 import { getProductsInCategory } from "@/Services/Product/product.service";
 import { Categorytemplate } from "@/ui-core";
 import { Metadata } from "next";
 import React from "react";
 
-export const metadata: Metadata = {
-  title: "Men's Clothes",
-  description:
-    "Explore the latest trends in men's fashion at Modern-walk. Find a wide range of stylish men's clothes and accessories",
-};
-
 async function MensFashion() {
   const data = await getProductsInCategory("men's clothing");
   return (
     <div className="pt-24">
-      <Categorytemplate products={data} title="Mens Fashion" />
+      <Categorytemplate
+        products={data}
+        title="Mens Fashion"
+        loading={false}
+        error={null}
+      />
     </div>
   );
 }
